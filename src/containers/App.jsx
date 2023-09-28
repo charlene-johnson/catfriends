a20ad87aeb0a23e59020d8881e7736a764e000b7
charlene-johnson <charlene.j8234@gmail.com>
2023-09-27 21:00:47 -0400

    added hooks and function instead of class

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,50 +1,41 @@
-import {Component} from 'react'
+import {useState, useEffect} from 'react'
 import CardList from '../components/CardList.jsx'
 import SearchBox from '../components/SearchBox.jsx'
 import Scroll from '../components/Scroll.jsx'
 import ErrorBoundary from '../components/ErrorBoundary.jsx'
 import './App.css';
 
+function App() {
+    const [cats, setCats] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+   
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            cats: [],
-            searchfield: ''
-        }
-    }
-
-    componentDidMount() {
+    useEffect(()=> {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({cats: users}));
+            .then(users => {setCats(users)})
+    }, []) 
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
-
-    render() {
-        const {cats, searchfield } = this.state
-        const filteredCats = cats.filter(cat => {
-            return cat.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        return !cats.length ? 
+    const filteredCats = cats.filter(cat => {
+        return cat.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !cats.length ? 
         <h1>Loading</h1> :
-            (
-                <div className='tc'>
-                    <h1 className='f-subheadline'>Cat Friends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundary>
-                             <CardList cats={filteredCats}/>
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            )
-        }
+        (
+            <div className='tc'>
+                 <h1 className='f-subheadline'>Cat Friends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                 <Scroll>
+                     <ErrorBoundary>
+                         <CardList cats={filteredCats}/>
+                    </ErrorBoundary>
+                </Scroll>
+            </div>
+        )
     }
-
-
+    
 export default App
---
+++ src/containers/OldApp.jsx
@@ -0,0 +1,50 @@
+import {Component} from 'react'
+import CardList from '../components/CardList.jsx'
+import SearchBox from '../components/SearchBox.jsx'
+import Scroll from '../components/Scroll.jsx'
+import ErrorBoundary from '../components/ErrorBoundary.jsx'
+import './App.css';
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            cats: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({cats: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render() {
+        const {cats, searchfield } = this.state
+        const filteredCats = cats.filter(cat => {
+            return cat.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !cats.length ? 
+        <h1>Loading</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f-subheadline'>Cat Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundary>
+                             <CardList cats={filteredCats}/>
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+            )
+        }
+    }
+
+
+export default App
